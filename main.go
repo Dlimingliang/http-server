14259ca89a28957a4021b133c9ee56500112bbaa
limingliang <lml2dyy@gmail.com>
2021-11-19 09:59:42 +0800

    代码优化

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/golang/glog"
 	"net"
 	"net/http"
@@ -11,44 +10,45 @@ import (
 
 const (
 	XForwardedFor = "X-Forwarded-For"
-	XRealIP       = "X-Real-IP"
-	OK 			  = "200"
+	XRealIp       = "X-Real-IP"
+	OkStr         = "200"
+	Ok            = 200
 )
 
 func main() {
 	glog.V(0).Info("Starting http server...")
+	glog.Error("i am error message")
 	http.HandleFunc("/", defaultHandler)
 	http.HandleFunc("/healthz", healthyCheckHandler)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
-		fmt.Println("listen error", err.Error())
+		glog.Error("listen error", err.Error())
 		return
 	}
 }
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-
-	ip := remoteIp(r)
-	glog.V(0).Info("client ip:", ip)
 	for name, headers := range r.Header {
 		for _, h := range headers {
 			w.Header().Set(name, h)
 		}
 	}
 	w.Header().Set("VERSION", os.Getenv("VERSION"))
+	ip := remoteIp(r)
+	glog.V(0).Info("client ip:", ip)
 	resp, _ := json.Marshal(map[string]string{
-		"ip": ip, "statusCode": OK,
+		"ip": ip, "statusCode": OkStr,
 	})
 	w.Write(resp)
 }
 
 func healthyCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
+	w.WriteHeader(Ok)
 }
 
 func remoteIp(req *http.Request) string {
 	remoteAddr := req.RemoteAddr
-	if ip := req.Header.Get(XRealIP); ip != "" {
+	if ip := req.Header.Get(XRealIp); ip != "" {
 		remoteAddr = ip
 	} else if ip = req.Header.Get(XForwardedFor); ip != "" {
 		remoteAddr = ip
